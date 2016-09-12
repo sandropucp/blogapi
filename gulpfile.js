fcc05f37de1e7f67487e6644d4ac2addf1f206ce
Sandro Sanchez <ssanchez@shelvingrock.local>
2016-09-12 15:47:17 -0400

    update 7

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,22 @@ var gulp = require('gulp'),
     supertest = require('supertest');
 
 
+// gulp.task('default', function(){
+//     nodemon({
+//         script: 'app.js',
+//         ext: 'js',
+//         env: {
+//             PORT:8000
+//         },
+//         ignore: ['./node_modules/**']
+//     })
+//     .on('restart', function(){
+//         console.log('Restarting');
+//     });
+// });
+
 gulp.task('default', function(){
-    nodemon({
-        script: 'app.js',
-        ext: 'js',
-        env: {
-            PORT:8000
-        },
-        ignore: ['./node_modules/**']
-    })
-    .on('restart', function(){
-        console.log('Restarting');
-    });
+	require('./app.js');
 });
 
 gulp.task('test', function(){

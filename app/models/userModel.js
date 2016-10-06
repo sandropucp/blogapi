1c10e0fc0dca53b17cbdef10f3c2f3d13758ad82
Sandro Sanchez <sandropucp@gmail.com>
2016-10-05 22:32:52 -0400

    fix number

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -5,7 +5,7 @@ var userModel = new Schema({
     name: { type: String, default: '' },
     email: { type: String, default: '' },        
     password: { type: String, default: '' },        
-    birthYear: { type: number, default: 0 },        
+    birthYear: { type: Number, default: 0 },        
 });
 
 module.exports = mongoose.model('User', userModel);
